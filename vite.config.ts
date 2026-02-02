dc3260f694aaf4faed336ac7d61360f47e80f292
Ahmed <ahmed@example.com>
2026-02-02 22:15:13 +0200

    Migration: Fresh deploy to important-project-2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/important-project/', // Match repository name for GH Pages
+    base: '/important-project-2/', // Match new repository name for GH Pages
     server: {
       port: 3000,
       host: '0.0.0.0',
